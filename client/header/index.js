48e74ef109cad9aff0f6cfcb3d404da3273739de
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-01-12 19:29:37 +0900

    [#2] UI: 헤더에 아이콘 추가
    - 홈 아이콘
    - 메시지 아이콘
    - 업로드 아이콘
    - 내 정보 아이콘

--- client/header/index.js
+++ client/header/index.js
@@ -1,5 +1,9 @@
 import logoImg from '../public/instargram.png';
 import searchImg from '../public/search.png';
+import homeImg from '../public/home.png';
+import dmImg from '../public/dm.png';
+import plusImg from '../public/plus.png';
+import userImg from '../public/user.png';
 import style from './style.scss';
 
 const Header = ({ $target, initialState }) => {
@@ -13,20 +17,21 @@ const Header = ({ $target, initialState }) => {
   const render = () => {
     $target.innerHTML = `
       <header>
-        <img class="header-logo" src=${logoImg} alt="ingstargram"></img>
-        <div class="searchBar">
-          <div class="search-logo-wrap">
-            <img class="search-logo" src=${searchImg} alt="search"></img>
-            <span>검색</span>
-          </div>  
-          <input type="text" placeholder="검색"></input>
-        </div>
         <nav>
+          <img class="header-logo" src=${logoImg} alt="ingstargram"></img>
+          <div class="searchBar">
+            <div class="search-logo-wrap">
+              <img class="search-logo" src=${searchImg} alt="search"></img>
+              <span>검색</span>
+            </div>  
+            <input type="text" placeholder="검색"></input>
+          </div>
+          
           <ul>
-            <li>home</li>
-            <li>dm</li>
-            <li>upload</li>
-            <li>mypage</li>
+            <li><img class="logo-item" src=${homeImg} alt="home"></img></li>
+            <li><img class="logo-item" src=${dmImg} alt="message"></img></li>
+            <li><img class="logo-item" src=${plusImg} alt="upload"></img></li>
+            <li><img class="logo-item" src=${userImg} alt="user"></img></li>
           </ul>
         </nav>
       </header>
